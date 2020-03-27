c8455ba1f32d57f548460ae7c553f40bbb44d8b6
littleredtootle <littleredtootle@gmail.com>
2020-03-27 18:41:12 +1300

    Slight update to evening time code

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ var noon = 12;
 var evening = 18; // 6PM
 var wakeupTime = 9; // 9AM
 var lunchTime = 12; // 12PM
-var partyTime = 17; // 5PM
+var partyTime; // 5PM
 var napTime = lunchTime + 2; // 2PM
 var time = new Date().getHours();
  
@@ -36,7 +36,7 @@ var updateClock = function(){
           messageText = "IZ TIME TO GETTUP.";
      } else if (time < noon) {
           messageText = "Good morning!";
-     } else if (time > evening) {
+     } else if (time >= evening) {
           messageText = "Good Evening!";
      }else {
           messageText = "Good afternoon!";
